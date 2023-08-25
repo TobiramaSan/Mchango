4087d58908e879c82aee122c9f89a7fc67e17b9d
3illBaby <thrillanon@outlook.com>
2023-08-25 11:32:58 +0100

    Added functions

--- src/components/upgradeHero/upgradeHero.jsx
+++ src/components/upgradeHero/upgradeHero.jsx
@@ -1,10 +1,24 @@
-import React from "react";
-import "./upgradeHero.css";
-import Basic from "../../assets/Basic.png";
-import Premium from "../../assets/Premium.png";
-import Exclusive from "../../assets/Exclusive.png";
-
+import React, { useState } from 'react';
+import './upgradeHero.css';
+import Basic from '../../assets/Basic.png';
+import Premium from '../../assets/Premium.png';
+import Exclusive from '../../assets/Exclusive.png';
+import { subscribeExclusive, subscribePremium } from '../../utils/payment';
 const UpgradeHero = () => {
+  const [feedBackMessage, setFeedBackMessage] = useState('');
+
+  const handlePremium = async () => {
+    try {
+      const amount = prompt('Enter the subscription amount in Ether:'); //
+      if (!amount) return;
+      const feedback = await subscribePremium(amount);
+      setFeedBackMessage(feedback);
+      console.log(feedback);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <div className="upgrade-hero">
       <div className="upgrade-hero-cont">
@@ -28,13 +42,13 @@ const UpgradeHero = () => {
             </div>
           </div>
         </div>
-        <div className="upgrade-main">
+        <div className="upgrade-main" onClick={handlePremium}>
           <div className="upgrade-image">
             <img src={Premium} alt="" />
           </div>
           <div className="upgrade-text">
             <div className="upgrade-text-title">
-              <h1>Basic Plan</h1>
+              <h1>Premium Plan</h1>
               <p>
                 The Basic Plan is ideal for users new to collective savings.
                 Start your journey towards financial goals with group savings
@@ -45,6 +59,7 @@ const UpgradeHero = () => {
               <p>Commission Fee: 3%</p>
               <p>No of subscriibers: 10 max</p>
               <p>Subscription Payment: 0 ETH</p>
+              <p>{feedBackMessage}</p>
             </div>
           </div>
         </div>
--- src/pages/landing/landing.js
+++ src/pages/landing/landing.js
@@ -1,9 +1,57 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
+import { checkAndAddSepoliaNetwork } from '../../utils/AddSepolia';
 // import { NavLink } from "react-router-dom";
-import "./landing.css";
-import arrow from "../../assets/arrow_right_alt.png";
+import './landing.css';
+import arrow from '../../assets/arrow_right_alt.png';
 
 const Landing = () => {
+  const [isConnected, setIsConnected] = useState(false);
+  const [account, setAccount] = useState('');
+  const connectWallet = async (event) => {
+    event.preventDefault();
+    try {
+      const accounts = await window.ethereum.request({
+        method: 'eth_requestAccounts',
+      });
+
+      if (accounts.length > 0) {
+        setAccount(accounts[0]);
+        setIsConnected(true);
+        console.log(`Selected Account: ${accounts[0]}`);
+      } else {
+        setIsConnected(false); // Set isConnected to false if no accounts are returned
+      }
+    } catch (error) {
+      setIsConnected(false);
+      setAccount('');
+      console.log(`Error: ${error.message}`);
+    }
+  };
+
+  const disconnectWallet = () => {
+    setIsConnected(false);
+    setAccount('');
+  };
+
+  useEffect(() => {
+    checkAndAddSepoliaNetwork();
+  }, []);
+
+  useEffect(() => {
+    if (!isConnected) {
+      console.log('Wallet disconnected');
+    }
+  }, [isConnected]);
+
+  const renderWalletAddress = () => {
+    if (isConnected && typeof account === 'string') {
+      const firstSixDigits = account.substring(0, 6);
+      const lastSixDigits = account.substring(account.length - 6);
+      return <span>{`${firstSixDigits}...${lastSixDigits}`}</span>;
+    }
+    return null;
+  };
+
   return (
     <div className="landing__pg">
       <div className="landing_pg__cont">
@@ -29,7 +77,9 @@ const Landing = () => {
               <a href="/signup">Sign Up</a>
             </li>
             <li className="contact__btn">
-              <a href="/">Connect Your Wallet</a>
+              <a href="/" onClick={(event) => connectWallet(event)}>
+                {isConnected ? renderWalletAddress() : 'Connect Wallet'}
+              </a>
               <img src={arrow} alt="" />
             </li>
           </ul>
